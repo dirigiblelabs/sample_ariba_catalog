fada9ee54d7e73db4a1f72eade95b7656a2d5a39
cviktorov <cviktorov@uni-sofia.bg>
2017-02-28 12:07:24 +0200

    Login form added

--- sample_ariba_catalog/ScriptingServices/sample_ariba_catalog/catalog_search.js
+++ sample_ariba_catalog/ScriptingServices/sample_ariba_catalog/catalog_search.js
@@ -7,20 +7,20 @@ var request = require('net/http/request');
 var response = require('net/http/response');
 
 var search = request.getParameter("search");
+var login = request.getParameter("user");
+var password = request.getParameter("password");
 if (search === null) {
 	response.println("search parameter required");
 	response.flush();
 	response.close();
 } else {
+
+	var vrealm = "<vrealm>";
+	var userId = "<userid>";
+	var server = "<server>";
+	var instance = "<instance>";
+
 	var count = request.getParameter("count");
-	
-	var vrealm = "<VREALM>";
-	var userId = "<USER>";
-	var login = "<LOGIN>";
-	var password = "<PASSWORD>";
-	var server = "<SERVER>";
-	var instance = "<INSTANCE>";
-	
 	var requestMessage = soap.createMessage();
 	var part = requestMessage.getPart();
 	var envelope = part.getEnvelope();
@@ -41,52 +41,65 @@ if (search === null) {
 	mimeHeaders.addHeader("Authorization", "Basic " + login + ":" + password);
 	
 	requestMessage.save();
+	var responseMessage;
+	try {
+   		responseMessage = soap.call(requestMessage, "https://" + server + ".ariba.com/Buyer/soap/" + instance + "/WSCatalogItemSearch");
+   		var envelopeJson = xml.toJson(responseMessage.getText());
 	
-	var responseMessage = soap.call(requestMessage, "https://" + server + ".ariba.com/Buyer/soap/" + instance + "/WSCatalogItemSearch");
+		var envelopeObject = JSON.parse(envelopeJson);
+		var result = [];
+		
+		var itemElement = envelopeObject["soap:Envelope"]["soap:Body"]["WSCatalogItemSearchReply"]["WSCatalogSearchResponse_Item"]["item"]["CatalogItems"]["item"];
+		var errorElement = envelopeObject["soap:Envelope"]["soap:Body"]["WSCatalogItemSearchReply"]["WSCatalogSearchResponse_Item"]["item"]["Errors"]["item"];
+		
+		console.log(errorElement);
+		var resultCount = 0;
+		if (Array.isArray(itemElement)) {
+			for (var i=0; i<itemElement.length; i++) {
+				var item = {};
+				item.manufacturerPartId = itemElement[i]["ManufacturerPartId"];
+				item.shortName = itemElement[i]["ShortName"];
+				item.image = itemElement[i]["Image"];
+				item.amount = itemElement[i]["Price"]["Amount"];
+				item.currency = itemElement[i]["Price"]["Currency"];
+				result.push(item);
+				resultCount++;
+				if (i > 11) {
+					break;
+				}
 	
-	var envelopeJson = xml.toJson(responseMessage.getText());
-	
-	var envelopeObject = JSON.parse(envelopeJson);
-	var result = [];
-	
-	var itemElement = envelopeObject["soap:Envelope"]["soap:Body"]["WSCatalogItemSearchReply"]["WSCatalogSearchResponse_Item"]["item"]["CatalogItems"]["item"];
-	
-	var resultCount = 0;
-	if (Array.isArray(itemElement)) {
-		for (var i=0; i<itemElement.length; i++) {
+			}
+		} else if(itemElement !== null && itemElement != undefined) {
 			var item = {};
-			item.manufacturerPartId = itemElement[i]["ManufacturerPartId"];
-			item.shortName = itemElement[i]["ShortName"];
-			item.image = itemElement[i]["Image"];
-			item.amount = itemElement[i]["Price"]["Amount"];
-			item.currency = itemElement[i]["Price"]["Currency"];
+			item.manufacturerPartId = itemElement["ManufacturerPartId"];
+			item.shortName = itemElement["ShortName"];
+			item.image = itemElement["Image"];
+			item.ammount = itemElement["Price"]["Amount"];
+			item.currency = itemElement["Price"]["Currency"];
 			result.push(item);
 			resultCount++;
-			if (i > 11) {
-				break;
-			}
-
+		} else{
+			result = errorElement;
 		}
-	} else {
-		var item = {};
-		item.manufacturerPartId = itemElement["ManufacturerPartId"];
-		item.shortName = itemElement["ShortName"];
-		item.image = itemElement["Image"];
-		item.ammount = itemElement["Price"]["Amount"];
-		item.currency = itemElement["Price"]["Currency"];
-		result.push(item);
-		resultCount++;
-	}
 	
-	
-	if (count !== null) {
-		response.println("12");
-	} else {
-		response.setContentType("text/plain; charset=UTF-8");
-		response.println( JSON.stringify(result) );
+		
+		
+		if (count !== null) {
+			response.println("12");
+		} else {
+			response.setContentType("text/plain; charset=UTF-8");
+			response.println( JSON.stringify(result) );
+		}
+		response.flush();
+		response.close();
+		
 	}
-	response.flush();
-	response.close();
+	catch(err) {
+		response.println("Unauthorized");
+		response.flush();
+		response.close();
+	}
+
 	
 
 }
